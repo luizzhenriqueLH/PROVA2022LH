1829b251b9a93d96c0fa62f6cfe22859be72b588
Luiz <luiz.marcondes.carneiro@esscola.pr.gov.br>
2026-09-01 07:23:32 -0300

    Removi as bordas externas

--- 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
+++ 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
@@ -30,7 +30,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (276, 390, largura - 276, altura - 280) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (130, 262, largura - 131, altura - 160) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
